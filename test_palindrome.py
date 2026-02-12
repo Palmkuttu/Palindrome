8200887a663c09408365b84c46ceba4b2dc7ee3f
Dennis Zacharia <dennyzach15@gmail.com>
2026-02-12 14:18:41 -0500

    Clean up test_palindrome.py by removing blank lines
    Removed unnecessary blank lines in test cases.

--- test_palindrome.py
+++ test_palindrome.py
@@ -6,10 +6,8 @@ def test_raises_value_error_when_not_string():
     with pytest.raises(ValueError):
         is_palindrome(123)
 
-
 def test_empty_string_returns_false():
     assert is_palindrome("") is False
-
 
 def test_single_character_returns_true():
     assert is_palindrome("a") is True
@@ -18,10 +16,8 @@ def test_single_character_returns_true():
 def test_two_same_letters_returns_true():
     assert is_palindrome("bb") is True
 
-
 def test_simple_false_case():
     assert is_palindrome("abc") is False
-
 
 def test_simple_true_case():
     assert is_palindrome("laval") is True
@@ -30,10 +26,8 @@ def test_simple_true_case():
 def test_non_palindrome_word():
     assert is_palindrome("toronto") is False
 
-
 def test_phrase_palindrome():
     assert is_palindrome("Able was I ere I saw Elba") is True
-
 
 def test_case_insensitive():
     assert is_palindrome("RaceCar") is True
