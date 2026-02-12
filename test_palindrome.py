23e662c348feb86559afadcdb81be228cdd22e9c
Dennis Zacharia <dennyzach15@gmail.com>
2026-02-12 14:31:11 -0500

    Add tests for is_palindrome function

--- test_palindrome.py
+++ test_palindrome.py
@@ -1,8 +1,10 @@
 import pytest
 from palindrome import is_palindrome
+
 def test_raises_value_error_when_not_string():
     with pytest.raises(ValueError):
         is_palindrome(123)
+        
 def test_empty_string_returns_false():
     assert is_palindrome("") is False
     
