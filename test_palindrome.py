9c5d114e613911bf7f0d152440cc2d9100467dff
Dennis Zacharia <dennyzach15@gmail.com>
2026-02-13 12:28:32 -0500

    Update test_palindrome.py

--- test_palindrome.py
+++ test_palindrome.py
@@ -3,3 +3,5 @@ def test_true():
     assert palindrome.is_palindrome("laval") is True
 def test_false():
     assert palindrome.is_palindrome("toronto") is False
+def test_phrase_palindrome():
+    assert is_palindrome("Able was I ere I saw Elba") is True
